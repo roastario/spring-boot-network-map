2e28114eba9186fd47bf6c5067fa4a744a89cf9e
Stefano Franz <stefano.franz@r3.com>
2018-04-05 11:58:36 +0100

    add file:/// urlprefix for finding notaries in docker

--- src/main/shell/start.sh
+++ src/main/shell/start.sh
@@ -3,4 +3,4 @@
 echo "starting network map service"
 
 # start the server
-exec java -Djava.security.egd=file:/dev/urandom -jar /app.jar --nodesDirectoryUrl=/opt/nodes
+exec java -Djava.security.egd=file:/dev/urandom -jar /app.jar --nodesDirectoryUrl=file:///opt/nodes
