1d1497bb817ae48e95b85f13f0e16efbd0e1f3c4
Stefano Franz <stefano.franz@r3.com>
2018-06-19 11:09:30 +0100

    working os3.1

--- src/main/kotlin/net/corda/network/map/SerializationEngine.kt
+++ src/main/kotlin/net/corda/network/map/SerializationEngine.kt
@@ -8,11 +8,11 @@ package net.corda.network.map
 import net.corda.core.serialization.SerializationContext
 import net.corda.core.serialization.internal.SerializationEnvironmentImpl
 import net.corda.core.serialization.internal.nodeSerializationEnv
-import net.corda.serialization.internal.AMQP_P2P_CONTEXT
-import net.corda.serialization.internal.CordaSerializationMagic
-import net.corda.serialization.internal.SerializationFactoryImpl
-import net.corda.serialization.internal.amqp.SerializerFactory
-import net.corda.serialization.internal.amqp.amqpMagic
+import net.corda.core.utilities.ByteSequence
+import net.corda.nodeapi.internal.serialization.AMQP_P2P_CONTEXT
+import net.corda.nodeapi.internal.serialization.SerializationFactoryImpl
+import net.corda.nodeapi.internal.serialization.amqp.AbstractAMQPSerializationScheme
+import net.corda.nodeapi.internal.serialization.amqp.SerializerFactory
 import org.springframework.stereotype.Component
 
 @Component
@@ -22,22 +22,19 @@ class SerializationEngine {
             val classloader = this.javaClass.classLoader
             nodeSerializationEnv = SerializationEnvironmentImpl(
                     SerializationFactoryImpl().apply {
-                        registerScheme(object : net.corda.serialization.internal.amqp.AbstractAMQPSerializationScheme(emptyList()){
-                            override fun canDeserializeVersion(magic: CordaSerializationMagic, target: SerializationContext.UseCase): Boolean {
-                                return (magic == amqpMagic && target == SerializationContext.UseCase.P2P)
+                        registerScheme(object : AbstractAMQPSerializationScheme(emptyList()){
+                            override fun canDeserializeVersion(byteSequence: ByteSequence, target: SerializationContext.UseCase): Boolean {
+                                return target == SerializationContext.UseCase.P2P
                             }
-
                             override fun rpcClientSerializerFactory(context: SerializationContext): SerializerFactory {
                                 throw UnsupportedOperationException()
                             }
-
                             override fun rpcServerSerializerFactory(context: SerializationContext): SerializerFactory {
                                 throw UnsupportedOperationException()
                             }
                         })
                     },
                     p2pContext = AMQP_P2P_CONTEXT.withClassLoader(classloader)
-
             )
         }
     }
