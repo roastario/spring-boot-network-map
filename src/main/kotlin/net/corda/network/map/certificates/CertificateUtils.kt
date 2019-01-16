22c7c3d812028217f0b0bb5c179536febb877099
admin <stefano.franz@r3.com>
2019-01-16 17:45:21 +0000

    move to using X509Utilities

---
+++ src/main/kotlin/net/corda/network/map/certificates/CertificateUtils.kt
@@ -0,0 +1,107 @@
+package net.corda.network.map.certificates
+
+import net.corda.core.CordaOID
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.internal.toX500Name
+import net.corda.network.map.NetworkMapApi
+import net.corda.nodeapi.internal.DEV_CA_TRUST_STORE_PASS
+import net.corda.nodeapi.internal.DEV_ROOT_CA
+import net.corda.nodeapi.internal.crypto.*
+import org.bouncycastle.asn1.ASN1EncodableVector
+import org.bouncycastle.asn1.ASN1ObjectIdentifier
+import org.bouncycastle.asn1.ASN1Sequence
+import org.bouncycastle.asn1.DERSequence
+import org.bouncycastle.asn1.x509.*
+import org.bouncycastle.cert.bc.BcX509ExtensionUtils
+import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder
+import org.bouncycastle.jce.PrincipalUtil
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder
+import org.bouncycastle.pkcs.PKCS10CertificationRequest
+import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequest
+import java.io.ByteArrayOutputStream
+import java.math.BigInteger
+import java.security.cert.X509Certificate
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import java.util.*
+import javax.security.auth.x500.X500Principal
+
+class CertificateUtils {
+
+    companion object {
+
+        val provider = BouncyCastleProvider()
+
+        fun createDevNetworkMapCa(rootCa: CertificateAndKeyPair): CertificateAndKeyPair {
+            val keyPair = NetworkMapApi.generateKeyPair()
+            val cert = X509Utilities.createCertificate(
+                    CertificateType.NETWORK_MAP,
+                    rootCa.certificate,
+                    rootCa.keyPair,
+                    X500Principal("CN=BasicNetworkMap,O=R3 Ltd,L=London,C=GB"),
+                    keyPair.public
+            )
+            return CertificateAndKeyPair(cert, keyPair)
+        }
+
+        fun createDevDoormanCa(rootCa: CertificateAndKeyPair): CertificateAndKeyPair {
+            val keyPair = NetworkMapApi.generateKeyPair()
+            val cert = X509Utilities.createCertificate(
+                    CertificateType.INTERMEDIATE_CA,
+                    rootCa.certificate,
+                    rootCa.keyPair,
+                    X500Principal("CN=BasicDoorman,O=R3 Ltd,L=London,C=GB"),
+                    keyPair.public
+            )
+            return CertificateAndKeyPair(cert, keyPair)
+        }
+
+        fun generateTrustStore(): ByteArray = ByteArrayOutputStream().apply {
+            X509KeyStore(DEV_CA_TRUST_STORE_PASS).apply {
+                setCertificate("cordarootca", DEV_ROOT_CA.certificate)
+            }.internal.store(this, DEV_CA_TRUST_STORE_PASS.toCharArray())
+        }.toByteArray()
+
+
+        fun createAndSignNodeCACerts(caCertAndKey: CertificateAndKeyPair,
+                                     request: PKCS10CertificationRequest): X509Certificate {
+            val jcaRequest = JcaPKCS10CertificationRequest(request)
+            val type = CertificateType.NODE_CA
+            val nameConstraints = NameConstraints(
+                    arrayOf (GeneralSubtree(GeneralName(
+                            GeneralName.directoryName,
+                            CordaX500Name.parse(jcaRequest.subject.toString()).copy(commonName = null).toX500Name()))),
+                    arrayOf()
+            )
+            val issuerCertificate = caCertAndKey.certificate
+            val issuerKeyPair = caCertAndKey.keyPair
+            val validityWindow = Date.from(Instant.now()) to Date.from(Instant.now().plus(500, ChronoUnit.DAYS))
+            val serial = BigInteger(128, Random())
+            val subject = X500Principal(CordaX500Name.parse(jcaRequest.subject.toString()).toX500Name().encoded)
+            val subjectPublicKeyInfo = SubjectPublicKeyInfo.getInstance(ASN1Sequence.getInstance(jcaRequest.publicKey.encoded))
+            val keyPurposes = DERSequence(ASN1EncodableVector().apply { type.purposes.forEach { add(it) } })
+
+            val partialBuilder = X509Utilities.createPartialCertificate(type, issuerCertificate.subjectX500Principal, issuerKeyPair.public, subject, jcaRequest.publicKey, validityWindow)
+
+            val builder = JcaX509v3CertificateBuilder(issuerCertificate, serial, validityWindow.first, validityWindow.second, subject, jcaRequest.publicKey)
+                    .addExtension(Extension.basicConstraints, type.isCA, BasicConstraints(type.isCA))
+                    .addExtension(Extension.subjectKeyIdentifier, false, BcX509ExtensionUtils().createSubjectKeyIdentifier(subjectPublicKeyInfo))
+                    .addExtension(Extension.extendedKeyUsage, false, keyPurposes)
+                    .addExtension(Extension.nameConstraints, true, nameConstraints)
+                    .addExtension(Extension.keyUsage, false, type.keyUsage)
+                    .addExtension(Extension.authorityKeyIdentifier, false, JcaX509ExtensionUtils().createAuthorityKeyIdentifier(issuerKeyPair.public))
+            if (type.role != null) {
+                builder.addExtension(ASN1ObjectIdentifier(CordaOID.X509_EXTENSION_CORDA_ROLE), false, type.role)
+            }
+            val signer = JcaContentSignerBuilder("SHA256withECDSA").setProvider(provider).build(issuerKeyPair.private)
+            val certificate = builder.build(signer).toJca()
+            certificate.checkValidity(Date())
+            certificate.verify(issuerKeyPair.public)
+            return certificate
+        }
+
+    }
+
+}
