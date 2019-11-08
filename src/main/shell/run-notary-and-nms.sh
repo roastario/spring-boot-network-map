2282c33e138e256e76ec4c3b4846b0ba4de64f9e
stefano <stefano.franz@r3.com>
2019-11-08 12:21:20 +0000

    use subshells

--- src/main/shell/run-notary-and-nms.sh
+++ src/main/shell/run-notary-and-nms.sh
@@ -4,12 +4,12 @@ CORDA_JAR_NAME="corda.jar"
 DOORMAN_CN_TO_USE=${DOORMAN_CN:-BasicDoorman}
 NETWORKMAP_CN_TO_USE=${NETWORKMAP_CN:-BasicNetworkMap}
 
-if [[ ! -d notary ]]; then
-  rm -rf notary
-  rm -rf spring-boot-network-map
+rm -rf notary
+rm -rf spring-boot-network-map
 
+(
   mkdir notary
-  cd notary
+  cd notary || exit
 
   if [[ ! -f ${CORDA_JAR_NAME} ]]; then
     wget -O ${CORDA_JAR_NAME} https://ci-artifactory.corda.r3cev.com/artifactory/corda-releases/net/corda/corda/4.0/corda-4.0.jar
@@ -35,15 +35,14 @@ devModeOptions{
 EOT
 
   java -jar ${CORDA_JAR_NAME} generate-node-info
+)
 
-  cd ..
-
+(
   git clone https://github.com/roastario/spring-boot-network-map.git
-  cd spring-boot-network-map
+  cd spring-boot-network-map || exit
   ./gradlew clean build
   cp build/libs/spring-boot-network-map-1.0-SNAPSHOT.jar ../nms.jar
-  cd ..
-fi
+)
 
 java -Djava.security.egd=file:/dev/urandom -jar nms.jar --nodesDirectoryUrl=file:notary/ --doormanCN="${DOORMAN_CN_TO_USE}" --networkMapCN="${NETWORKMAP_CN_TO_USE}" &
 NMS_PID=$!
@@ -56,26 +55,28 @@ while [ ${EXIT_CODE} -gt 0 ]; do
   let EXIT_CODE=$?
 done
 
-cd notary
-java -jar ${CORDA_JAR_NAME} &
-NOTARY_PID=$!
+(
+  cd notary || exit
+  java -jar ${CORDA_JAR_NAME} &
+  NOTARY_PID=$!
 
-echo "Started notary with PID=${NOTARY_PID}"
-echo "Started nms with PID=${NMS_PID}"
+  echo "Started notary with PID=${NOTARY_PID}"
+  echo "Started nms with PID=${NMS_PID}"
 
-function ctrl_c() {
-  echo "Killing notary ${NOTARY_PID}"
-  kill ${NOTARY_PID}
-  wait ${NOTARY_PID}
-  echo "Killing network-map-service ${NOTARY_PID}"
-  kill ${NMS_PID}
-  wait ${NMS_PID}
-  kill $$
-}
+  function ctrl_c() {
+    echo "Killing notary ${NOTARY_PID}"
+    kill ${NOTARY_PID}
+    wait ${NOTARY_PID}
+    echo "Killing network-map-service ${NOTARY_PID}"
+    kill ${NMS_PID}
+    wait ${NMS_PID}
+    kill $$
+  }
 
-# trap ctrl-c and call ctrl_c()
-trap ctrl_c INT
+  # trap ctrl-c and call ctrl_c()
+  trap ctrl_c INT
 
-while :; do
-  sleep 1
-done
+  while :; do
+    sleep 1
+  done
+)
