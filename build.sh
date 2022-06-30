25745035814f7dd061ecd86e94f7db8801a82e4d
Stefano Franz <roastario@gmail.com>
2022-06-30 13:28:18 +0200

    pre-rebase

--- build.sh
+++ build.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
-./gradlew clean build
-./gradlew copyCordaJar copyNetworkMapJar copyNotaryConfigs copyShell
+./gradlew clean build || exit
+./gradlew copyCordaJar copyNetworkMapJar copyNotaryConfigs copyShell || exit
 
 cd ./build/docker || exit
-docker build . -f ../../DockerfileNotaryOne -t notary_one || exit
-docker build . -f ../../DockerfileNotaryTwo -t notary_two || exit
-docker build . -f ../../DockerfileNMS -t network_services || exit
+docker build --build-arg NOTARY_NAME="O=BISNotary, L=Basel, C=CH" . -f ../../DockerfileNotary -t notary_one --no-cache || exit
+docker build --build-arg NOTARY_NAME="O=RBANotary, L=Sydney, C=AU" . -f ../../DockerfileNotary -t notary_two --no-cache || exit
+docker build --build-arg NOTARY_NAME="O=BNMNotary, L=KualaLumpur, C=MY" . -f ../../DockerfileNotary -t notary_three --no-cache || exit
+docker build --build-arg NOTARY_NAME="O=MASNotary, L=Singapore, C=SG" . -f ../../DockerfileNotary -t notary_four --no-cache || exit
+docker build --build-arg NOTARY_NAME="O=SARBNotary, L=Pretoria, C=ZA" . -f ../../DockerfileNotary -t notary_five --no-cache || exit
+docker build . -f ../../DockerfileNMS -t network_services --no-cache || exit
+#
+docker tag notary_one roastario/notary-one-fixed:7
+docker tag notary_two roastario/notary-two-fixed:7
+docker tag notary_three roastario/notary-three-fixed:7
+docker tag notary_four roastario/notary-four-fixed:7
+docker tag notary_five roastario/notary-five-fixed:7
+docker tag network_services roastario/network-services-fixed:7
+#
+docker push roastario/notary-one-fixed:7
+docker push roastario/notary-two-fixed:7
+docker push roastario/notary-three-fixed:7
+docker push roastario/notary-four-fixed:7
+docker push roastario/notary-five-fixed:7
 
-docker tag notary_one roastario/notary-one:latest
-docker tag notary_two roastario/notary-two:latest
-docker tag network_services roastario/network-services:latest
-
-docker push roastario/notary-one:latest
-docker push roastario/notary-two:latest
-docker push roastario/network-services:latest
+docker push roastario/network-services-fixed:7
--- src/main/kotlin/net/corda/network/map/repository/sqllite/SqlLiteRepository.kt
+++ src/main/kotlin/net/corda/network/map/repository/sqllite/SqlLiteRepository.kt
@@ -1,30 +1,137 @@
 package net.corda.network.map.repository.sqllite
 
+import net.corda.core.crypto.SecureHash
+import net.corda.core.serialization.deserialize
+import net.corda.core.serialization.serialize
+import net.corda.network.map.repository.NodeInfoRepository
+import net.corda.network.map.repository.SqlLiteBacked
+import net.corda.nodeapi.internal.SignedNodeInfo
+import org.springframework.stereotype.Component
 import org.sqlite.SQLiteDataSource
 import java.sql.Connection
 import java.util.concurrent.locks.ReentrantReadWriteLock
 import kotlin.concurrent.read
 import kotlin.concurrent.write
 
-abstract class SqlLiteRepository {
+@Component
+@SqlLiteBacked
+class SqlLiteRepository : NodeInfoRepository {
 
 
     companion object {
         internal val globalLock: ReentrantReadWriteLock = ReentrantReadWriteLock()
-        val jdbcUrl = "jdbc:sqlite::memory:"
+        val jdbcUrl = "jdbc:sqlite:/opt/node-storage/nm.db:"
         val dataSource: SQLiteDataSource = SQLiteDataSource().also { it.url = jdbcUrl }
+    }
+
+    init {
+        dataSource.write {
+            it.createStatement().use { statement ->
+                statement.execute(
+                    """
+                    CREATE TABLE IF NOT EXISTS NODEINFOS_BY_HASH
+                    (
+                        hash text NOT NULL PRIMARY KEY,
+                        value BLOB NOT NULL
+                    )
+                """.trimIndent()
+                )
+            }
+
+            it.createStatement().use { statement ->
+                statement.execute(
+                    """
+                    CREATE TABLE IF NOT EXISTS HASH_BY_X500
+                    (
+                        x500 text NOT NULL PRIMARY KEY,
+                        hash text NOT NULL
+                    )
+                    """.trimIndent()
+                )
+
+            }
+        }
     }
 
 
     fun <T> SQLiteDataSource.read(block: (ds: Connection) -> T): T {
         globalLock.read {
-            return dataSource.connection.use(block)
+            return this.connection.use(block)
         }
     }
 
     fun <T> SQLiteDataSource.write(block: (ds: Connection) -> T): T {
         globalLock.write {
-            return dataSource.connection.use(block)
+            return this.connection.use(block)
+        }
+    }
+
+    override fun persistSignedNodeInfo(signedNodeInfo: SignedNodeInfo) {
+        dataSource.write {
+            it.autoCommit = false
+            it.prepareStatement(
+                """
+                INSERT INTO NODEINFOS_BY_HASH (HASH, VALUE) 
+                VALUES (?,?)
+                ON CONFLICT(HASH)
+                DO UPDATE SET VALUE=excluded.VALUE;
+                """.trimIndent()
+            ).use { statement ->
+                statement.setString(1, signedNodeInfo.raw.hash.toString())
+                statement.setBytes(2, signedNodeInfo.serialize().bytes)
+                statement.execute()
+            }
+            it.prepareStatement(
+                """
+                INSERT INTO HASH_BY_X500 (X500, HASH) 
+                VALUES (?,?)
+                ON CONFLICT(X500)
+                DO UPDATE SET HASH=excluded.HASH;                       
+                """.trimIndent()
+            ).use {statement ->
+                statement.setString(1, signedNodeInfo.verified().legalIdentities.first().name.toString())
+                statement.setString(2, signedNodeInfo.raw.hash.toString())
+                statement.execute()
+            }
+            it.commit()
+        }
+    }
+
+    override fun getSignedNodeInfo(hash: String): Pair<SignedNodeInfo, ByteArray>? {
+        return dataSource.read {
+            it.prepareStatement("SELECT hash, value FROM NODEINFOS_BY_HASH where hash = ? ").use { statement ->
+                statement.setString(1, hash)
+                val rs = statement.executeQuery()
+                if (rs.next()) {
+                    val bytes = rs.getBytes(2)
+                    val signedNodeInfo = bytes.deserialize<SignedNodeInfo>()
+                    signedNodeInfo to bytes
+                } else {
+                    null
+                }
+            }
+        }
+    }
+
+    override fun getAllHashes(): Collection<SecureHash> {
+        return dataSource.read {
+            it.prepareStatement("SELECT hash FROM HASH_BY_X500").use { statement ->
+                val resultSet = statement.executeQuery()
+                resultSet.use {
+                    generateSequence {
+                        if (resultSet.next()) SecureHash.parse(resultSet.getString(1)) else null
+                    }.toList()
+                }
+            }
+        }
+    }
+
+    override fun purgeAllPersistedSignedNodeInfos(): Int {
+        return dataSource.write {
+            it.createStatement().use { statement ->
+                statement.executeUpdate("TRUNCATE TABLE NODEINFOS_BY_HASH")
+                statement.executeUpdate("TRUNCATE TABLE HASH_BY_X500")
+            }
         }
     }
 
